4c8970c20ac9f3497cc2c6681d21b55d26b4551c
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 05:48:19 +0600

    Express JS – Cookie Response

--- Routing/index.js
+++ Routing/index.js
@@ -2,13 +2,14 @@ var expressJS = require('express');
 
 app = expressJS();
 
-app.get("/header", function (request,response){
+app.get("/cookie", function (request,response){
 
-    response.append('name','Nirob');
-    response.append('city','Dhaka');
-    response.append('age','23');
+    response.cookie('name','Nirob');
+    response.cookie('city','Dhaka');
+    response.cookie('age','23');
 
-    response.status(201).end("This is Body");
+    response.status(201).end("Cookie Set Success");
+
 })
 
 
