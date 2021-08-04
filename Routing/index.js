551aa1635c4c719ea6ef87952cc7fbd8337014ce
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 05:39:58 +0600

    Express JS – Header Response

--- Routing/index.js
+++ Routing/index.js
@@ -2,13 +2,16 @@ var expressJS = require('express');
 
 app = expressJS();
 
-app.get("/nirob", function (request,response){
-    response.redirect("/hasib");
+app.get("/header", function (request,response){
+
+    response.append('name','Nirob');
+    response.append('city','Dhaka');
+    response.append('age','23');
+
+    response.send("This is Body");
+    response.status(201).end();
 })
 
-app.get("/hasib", function (request,response){
-    response.send("Hasibul Pages");
-})
 
 app.listen(3000,function (){
     console.log("Server Run Success");
