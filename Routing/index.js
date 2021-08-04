4dc679affad35aca221ea4ebf1573868a1b53212
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 05:08:01 +0600

    Express JS Status Code

--- Routing/index.js
+++ Routing/index.js
@@ -2,20 +2,9 @@ var expressJS = require('express');
 
 app = expressJS();
 
-app.get("/", function (request,response){
-    response.send("This is Home Page");
-})
-
-app.post("/about", function (request,response){
-    response.send("This is About Page");
-})
-
-app.put("/contact", function (request,response){
-    response.send("This is Contact Page");
-})
-
-app.delete("/phone", function (request,response){
-    response.send("This is Phone Page");
+app.get("/stCode", function (request,response){
+    response.status(200).end();
+    //response.send("This is Phone Page");
 })
 
 app.listen(3000,function (){
