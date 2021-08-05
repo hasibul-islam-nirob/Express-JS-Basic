ed103666967292b8c314f46a9ab2e6fa5a6b1084
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 06:35:44 +0600

    Express JS – Request Method Header

--- Request/index.js
+++ Request/index.js
@@ -6,8 +6,9 @@ app.get("/", function (request,response){
 
     let fastname = request.header('fname');
     let lastname = request.header('lname');
+    let UserAgent = request.header('User-Agent');
 
-    response.end(fastname +" "+lastname);
+    response.end(fastname +" "+lastname+" "+UserAgent);
 
 })
 
