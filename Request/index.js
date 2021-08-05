b146f1289c7a30e3ee686cb9c404aa0b419903bf
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 09:51:15 +0600

    Express JS – Post Request File Upload By Multer

--- Request/index.js
+++ Request/index.js
@@ -1,25 +1,35 @@
 var expressJS = require('express');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 var multer = require('multer');
 
 var multerInc = multer();
 var app = expressJS();
 
 // parse application/json
-app.use(bodyParser.json());
-app.use(multerInc.array());
-app.use(expressJS.static('public'));
+//app.use(bodyParser.json());
+//app.use(multerInc.array());
+//app.use(expressJS.static('public'));
 
+var myStorage = multer.diskStorage({
+        destination:function (request, file, callBack){
+            callBack(null,'./upload')
+        },
+        filename:function (request, file, callBack){
+            callBack(null, file.originalname)
+        }
+    })
+
+var upload = multer({storage:myStorage}).single('myfile');
 
 app.post("/", function (request,response){
 
-    let jsonData = request.body;
-    let jsonString = JSON.stringify(jsonData);
-    //let name = jsonData['name'];
-    //let city = jsonData['city'];
-
-    response.send(jsonString);
-    //response.send(name+"  "+city);
+    upload(request, response, function (error) {
+        if (error){
+            response.send("File Upload Fail");
+        }else {
+            response.send("File Upload Success");
+        }
+    })
 
 })
 
