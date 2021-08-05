c615405f35c61d357c890632e619fb2c618ff537
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 10:24:28 +0600

    Express JS – Middleware (Routing Level)

--- Middleware/index.js
+++ Middleware/index.js
@@ -2,20 +2,25 @@ var expressJS = require('express');
 
 var app = expressJS();
 
-app.use(function (request,response, next) {
-    console.log("I'm From Application Middleware");
-    next();
+app.get("/", function (request,response){
+    response.send("This is Home Page");
 })
 
 
-app.get("/", function (request,response){
-    response.send("This is Home Page");
+app.use("/about", function (request,response, next) {
+    console.log("I'm From About Middleware");
+    next();
 })
 
 app.get("/about", function (request,response){
     response.send("This is About Page");
 })
 
+
+app.use("/contact", function (request,response, next) {
+    console.log("I'm From Contact Middleware");
+    next();
+})
 app.get("/contact", function (request,response){
     response.send("This is Contact Page");
 })
