a5daa3127154895a8c6aa20e79b08ab71e692aaa
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-05 09:19:08 +0600

    Express JS – Post Request Multi-Part Form Data

--- Request/index.js
+++ Request/index.js
@@ -1,19 +1,25 @@
 var expressJS = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
+var multerInc = multer();
 var app = expressJS();
+
 // parse application/json
 app.use(bodyParser.json());
+app.use(multerInc.array());
+app.use(expressJS.static('public'));
+
 
 app.post("/", function (request,response){
 
     let jsonData = request.body;
-    //let jsonString = JSON.stringify(jsonData);
-    let name = jsonData['name'];
-    let city = jsonData['city'];
+    let jsonString = JSON.stringify(jsonData);
+    //let name = jsonData['name'];
+    //let city = jsonData['city'];
 
-    //response.send(jsonString);
-    response.send(name+"  "+city);
+    response.send(jsonString);
+    //response.send(name+"  "+city);
 
 })
 
